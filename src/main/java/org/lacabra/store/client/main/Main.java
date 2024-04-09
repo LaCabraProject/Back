58d75632cffe80548d88eb26c1dfddbe29187cfb
dnocitomerodio <daniel.nocito@opendeusto.es>
2024-04-09 17:01:52 +0200

    main nombre corrección

--- src/main/java/org/lacabra/store/client/main/Main.java
+++ src/main/java/org/lacabra/store/client/main/Main.java
@@ -2,7 +2,7 @@ package org.lacabra.store.client.main;
 
 import org.lacabra.store.client.windows.WindowHome;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         WindowHome wh=new WindowHome(null);
     }
