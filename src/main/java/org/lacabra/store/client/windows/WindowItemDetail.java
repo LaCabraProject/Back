f025d7774e32c8cc0ab270aee0af5a226cfec3bb
gpb117 <gonzalo.pena@opendeusto.es>
2024-04-29 13:37:08 +0200

    nueva version

--- src/main/java/org/lacabra/store/client/windows/WindowItemDetail.java
+++ src/main/java/org/lacabra/store/client/windows/WindowItemDetail.java
@@ -10,6 +10,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class WindowItemDetail extends JFrame {
+
     public WindowItemDetail() {
         super("Detalle del Artículo");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,6 +20,26 @@ public class WindowItemDetail extends JFrame {
         panelPrincipal.setLayout(new BorderLayout());
         panelPrincipal.setBorder(new EmptyBorder(10, 10, 10, 10));
 
+        JPanel barraNavegacion = crearBarraNavegacion();
+        panelPrincipal.add(barraNavegacion, BorderLayout.NORTH);
+
+        ImageIcon iconoImagen = new ImageIcon("ruta/a/imagen.jpg");
+        JLabel etiquetaImagen = new JLabel(iconoImagen);
+        etiquetaImagen.setPreferredSize(new Dimension(300, 300));
+        panelPrincipal.add(etiquetaImagen, BorderLayout.WEST);
+
+        JTabbedPane panelPestanas = crearPanelPestanas();
+        panelPrincipal.add(panelPestanas, BorderLayout.CENTER);
+
+        JPanel piePagina = crearPiePagina();
+        panelPrincipal.add(piePagina, BorderLayout.SOUTH);
+
+        add(panelPrincipal);
+
+        setVisible(true);
+    }
+
+    private JPanel crearBarraNavegacion() {
         JPanel barraNavegacion = new JPanel();
         barraNavegacion.setLayout(new GridLayout(1, 3));
         barraNavegacion.setBackground(Color.LIGHT_GRAY);
@@ -27,6 +48,7 @@ public class WindowItemDetail extends JFrame {
         botonInicio.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
             }
         });
         barraNavegacion.add(botonInicio);
@@ -35,6 +57,7 @@ public class WindowItemDetail extends JFrame {
         botonCategoria.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
             }
         });
         barraNavegacion.add(botonCategoria);
@@ -43,41 +66,39 @@ public class WindowItemDetail extends JFrame {
         botonCarrito.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
             }
         });
         barraNavegacion.add(botonCarrito);
 
-        panelPrincipal.add(barraNavegacion, BorderLayout.NORTH);
+        JButton botonBuscar = new JButton("Buscar");
+        botonBuscar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-        ImageIcon iconoImagen = new ImageIcon("ruta/a/imagen.jpg");
-        JLabel etiquetaImagen = new JLabel(iconoImagen);
-        etiquetaImagen.setPreferredSize(new Dimension(300, 300));
-        panelPrincipal.add(etiquetaImagen, BorderLayout.WEST);
+            }
+        });
+        barraNavegacion.add(botonBuscar);
 
+        return barraNavegacion;
+    }
+
+    private JTabbedPane crearPanelPestanas() {
         JTabbedPane panelPestanas = new JTabbedPane();
-        panelPrincipal.add(panelPestanas, BorderLayout.CENTER);
 
-        JPanel panelDesc = new JPanel();
-        panelDesc.setLayout(new BorderLayout());
-        panelDesc.add(new JLabel("Descripción del Producto"), BorderLayout.NORTH);
-        panelDesc.add(new JLabel("Este es un sombrero compacto y ligero perfecto para la playa, la montaña o la ciudad. Hecho de algodón 100% transpirable con agujeros de ventilación, cuenta con una parte superior plana y un ala de anchura media para protegerte del sol. La corona es sin estructura y mide 8 cm de profundidad. Fácil de mantener: simplemente lávalo y sécalo a la sombra"), BorderLayout.CENTER);
-        panelPestanas.addTab("Descripción", panelDesc);
+        JPanel panelResenas = new JPanel();
+        panelResenas.setLayout(new BorderLayout());
+        panelResenas.add(new JLabel("Reseñas del Producto"), BorderLayout.NORTH);
+        JTextArea areaResenas = new JTextArea();
+        areaResenas.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(areaResenas);
+        panelResenas.add(scrollPane, BorderLayout.CENTER);
+        panelPestanas.addTab("Reseñas", panelResenas);
 
+        return panelPestanas;
+    }
 
-        JPanel panelEspecificaciones = new JPanel();
-        panelEspecificaciones.setLayout(new GridLayout(6, 2));
-        panelEspecificaciones.add(new JLabel("Color:"));
-        panelEspecificaciones.add(new JLabel("Negro"));
-        panelEspecificaciones.add(new JLabel("Añadir al carrito:"));
-        panelEspecificaciones.add(new JButton("Añadir"));
-        panelEspecificaciones.add(new JLabel("Entrega:"));
-        panelEspecificaciones.add(new JLabel("Urgente: 7 de mayo, Ordinaria: 7-10 de mayo"));
-        panelEspecificaciones.add(new JLabel("Devoluciones fáciles:"));
-        panelEspecificaciones.add(new JLabel("Sí"));
-        panelEspecificaciones.add(new JLabel("Valoración:"));
-        panelEspecificaciones.add(new JLabel("4.20 (40 reseñas)"));
-        panelPestanas.addTab("Especificaciones", panelEspecificaciones);
-
+    private JPanel crearPiePagina() {
         JPanel piePagina = new JPanel();
         piePagina.setLayout(new GridLayout(1, 5));
         piePagina.setBackground(Color.LIGHT_GRAY);
@@ -87,6 +108,7 @@ public class WindowItemDetail extends JFrame {
         etiquetaEnlace1.addMouseListener((MouseListener) new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+
             }
         });
         piePagina.add(etiquetaEnlace1);
@@ -96,23 +118,47 @@ public class WindowItemDetail extends JFrame {
         etiquetaEnlace2.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+
             }
         });
         piePagina.add(etiquetaEnlace2);
 
-        JLabel etiquetaRedSocial1 = new JLabel(new ImageIcon("ruta/a/facebook.png"));
-        piePagina.add(etiquetaRedSocial1);
+        JButton botonFacebook = new JButton(new ImageIcon("ruta/a/facebook.png"));
+        botonFacebook.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-        JLabel etiquetaRedSocial2 = new JLabel(new ImageIcon("ruta/a/twitter.png"));
-        piePagina.add(etiquetaRedSocial2);
+            }
+        });
+        piePagina.add(botonFacebook);
 
-        JLabel etiquetaRedSocial3 = new JLabel(new ImageIcon("ruta/a/instagram.png"));
-        piePagina.add(etiquetaRedSocial3);
+        JButton botonTwitter = new JButton(new ImageIcon("ruta/a/twitter.png"));
+        botonTwitter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-        panelPrincipal.add(piePagina, BorderLayout.SOUTH);
+            }
+        });
+        piePagina.add(botonTwitter);
 
-        add(panelPrincipal);
+        JButton botonInstagram = new JButton(new ImageIcon("ruta/a/instagram.png"));
+        botonInstagram.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	
+            }
+        });
+        piePagina.add(botonInstagram);
 
-        setVisible(true);
+        return piePagina;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new WindowItemDetail();
+            }
+        });
     }
 }
